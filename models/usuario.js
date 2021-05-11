3b2845bc67934c87867067929ff9393bfb4709e7
lucashenrique01 <lucas.paula@bandtec.com.br>
2021-05-11 16:40:12 -0300

    Arquivo principal

---
+++ models/usuario.js
@@ -0,0 +1,45 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Usuario1 = sequelize.define('Usuario1',{
+		id: {
+			field: 'id',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nome: {
+			field: 'nome',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			field: 'email',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		cnpj: {
+			field: 'cnpj',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'usuario1', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Usuario1;
+};
